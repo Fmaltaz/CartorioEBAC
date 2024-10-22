971222c4e941cf3327817e7250de1f5bbfb51bb6
Fábio Malta <fmalta02@gmail.com>
2024-10-21 22:00:26 -0300

    Adição de funções para o usuário
    Possibilita o usuário registrar, consultar e apagar mais de um usuário por vez , sem precisar retornar ao menu principal

--- Cartório_EBAC.c
+++ Cartório_EBAC.c
@@ -13,54 +13,67 @@ int registrar() //Função responsável por registrar os usuários no sistema
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
+	char opcao;
 	//Fim da criação de variáveis
 	
-	printf("Digite o CPF do usuário a ser cadastrado: "); //Coleta informação do usuário
-	scanf("%s", cpf); //Armazena a string com o "%s"
+	do 
+	{
+
+		printf("Digite o CPF do usuário a ser cadastrado: "); //Coleta informação do usuário
+		scanf("%s", cpf); //Armazena a string com o "%s"
+		
+		strcpy(arquivo, cpf); //responsável por copiar o valor das strings
+		
+		FILE *file; //cria o arquivo 
+		file = fopen(arquivo, "w"); //cria o arquivo 
+		fprintf(file, "CPF: %s\n", cpf); //salva o valor da variável
+		fclose(file); //fecha o arquivo
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
+		fprintf(file, "Nome: "); //Adiciona o rótulo "nome" ao arquivo
+		fclose(file);//Fecha o arquivo
+		
+		printf("Digite o nome a ser cadastrado: "); //Coleta informação do usuário
+		scanf("%s", nome); //Armazena essa informação
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
+		fprintf(file, "%s\n", nome); //Adiciona o "nome" no arquivo
+		fclose(file); //Fecha o arquivo
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adiconar mais dados
+		fprintf(file, "Sobrenome: "); //Adiciona o rótulo "sobrenome" ao arquivo
+		fclose(file); //Fecha o arquivo
+		
+		printf("Digite o sobrenome a ser cadastrado: "); //Coleta informação do usuário
+		scanf("%s", sobrenome); //Armazena essa informação
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
+		fprintf(file,"%s\n", sobrenome); //Adiciona o "sobrenome" ao arquivo
+		fclose(file); //Fecha o arquivo
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
+		fprintf(file, "Cargo: "); //Adiciona o rótulo "cargo" ao arquivo
+		fclose(file); //Fecha o arquivo
+		
+		printf("Digite o cargo a ser cadastrado: "); //Coleta informação do usuário
+		scanf("%s", cargo); //Armazena essa informação
+		
+		file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais informação
+		fprintf(file,"%s\n", cargo); //Adiciona o "cargo" ao arquivo
+		fclose(file); //Fecha o arquivo
+		
+		printf("Usuário cadastrado com sucesso!\n"); //Exibe uma validação da açaõ de registro para o usuário
+		
+		printf("\nDeseja cadastrar outro usuário? (s/n): ");
+		scanf(" %c", &opcao);
+		system("cls");
 	
-	strcpy(arquivo, cpf); //responsável por copiar o valor das strings
+	}
+	while (opcao == 's');
+	return 0;
 	
-	FILE *file; //cria o arquivo 
-	file = fopen(arquivo, "w"); //cria o arquivo 
-	fprintf(file, "CPF: %s\n", cpf); //salva o valor da variável
-	fclose(file); //fecha o arquivo
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
-	fprintf(file, "Nome: "); //Adiciona o rótulo "nome" ao arquivo
-	fclose(file);//Fecha o arquivo
-	
-	printf("Digite o nome a ser cadastrado: "); //Coleta informação do usuário
-	scanf("%s", nome); //Armazena essa informação
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
-	fprintf(file, "%s\n", nome); //Adiciona o "nome" no arquivo
-	fclose(file); //Fecha o arquivo
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adiconar mais dados
-	fprintf(file, "Sobrenome: "); //Adiciona o rótulo "sobrenome" ao arquivo
-	fclose(file); //Fecha o arquivo
-	
-	printf("Digite o sobrenome a ser cadastrado: "); //Coleta informação do usuário
-	scanf("%s", sobrenome); //Armazena essa informação
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
-	fprintf(file,"%s\n", sobrenome); //Adiciona o "sobrenome" ao arquivo
-	fclose(file); //Fecha o arquivo
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais dados
-	fprintf(file, "Cargo: "); //Adiciona o rótulo "cargo" ao arquivo
-	fclose(file); //Fecha o arquivo
-	
-	printf("Digite o cargo a ser cadastrado: "); //Coleta informação do usuário
-	scanf("%s", cargo); //Armazena essa informação
-	
-	file = fopen(arquivo, "a"); //Abre o arquivo para adicionar mais informação
-	fprintf(file,"%s\n", cargo); //Adiciona o "cargo" ao arquivo
-	fclose(file); //Fecha o arquivo
-	
-	printf("Usuário cadastrado com sucesso!\n"); //Exibe uma validação da açaõ de registro para o usuário
-	system("pause"); //Pausa o programa até que o usuário pressione uma tecla
-	
+		
+		
 }
 
 int consultar() //Função responsável por consultar os usuários no sistema
@@ -70,30 +83,40 @@ int consultar() //Função responsável por consultar os usuários no sistema
 	//Inicio da criação de variáveis
 	char cpf[40];
 	char conteudo[200];
+	char opcao;
 	//Fim da criação de variáveis
 	
-	printf("Digite o CPF a ser consultado: "); //Coleta informação do usuário
-	scanf("%s", cpf); //Armazena essa informação
-	
-	FILE *file;
-	file = fopen(cpf, "r"); //Abre o arquivo corresponde ao CPF digitado no modo de leitura (read)
-	
-	if (file == NULL) //Verifica se o arquivo existe, se não, exibe mensagem de erro
-	{
-		printf ("CPF não encontrado\n"); //Mensagem de erro exibida para o usuário
-		system("pause"); //Pausa o programa até o usuário pressionar uma tecla
-		return 1; //Sai da função com um valor que indica erro
+	do {
+		
+		
+		printf("Digite o CPF a ser consultado: "); //Coleta informação do usuário
+		scanf("%s", cpf); //Armazena essa informação
+		
+		FILE *file;
+		file = fopen(cpf, "r"); //Abre o arquivo corresponde ao CPF digitado no modo de leitura (read)
+		
+		if (file == NULL) //Verifica se o arquivo existe, se não, exibe mensagem de erro
+		{
+			printf ("CPF não encontrado\n"); //Mensagem de erro exibida para o usuário
+			system("pause"); //Pausa o programa até o usuário pressionar uma tecla
+			return 1; //Sai da função com um valor que indica erro
+		}
+		
+		printf("\nInformações do usuário:\n"); //Exibe as informações do usuário se forem encontradas
+		
+		while (fgets(conteudo, 200,file) != NULL) //Lê cada linha do arquivo e exibe o conteúdo até chegar ao fim do arquivo
+		{
+			printf("%s", conteudo); //Exibe o conteudo do arquivo
+		}
+		
+		fclose(file);
+		
+		printf("\nDeseja consultar outro usuário? (s/n): ");
+		scanf(" %c", &opcao);
+		system("cls");
 	}
-	
-	printf("\nInformações do usuário:\n"); //Exibe as informações do usuário se forem encontradas
-	
-	while (fgets(conteudo, 200,file) != NULL) //Lê cada linha do arquivo e exibe o conteúdo até chegar ao fim do arquivo
-	{
-		printf("%s", conteudo); //Exibe o conteudo do arquivo
-	}
-	
-	fclose(file);
-	system("pause"); //Pausa o programa até o usuário pressionar uma tecla
+	while (opcao == 's');
+	return 0;
 }
 
 int apagar() //Função responsável por apagar os usuários do sistema
@@ -102,28 +125,38 @@ int apagar() //Função responsável por apagar os usuários do sistema
 	
 	//Inicio da criação de variáveis
 	char cpf[40];
+	char opcao;
 	//Fim da criação de variáveis
 	
-	printf("Digite o CPF do usuário a ser apagado: "); //Coleta informação do usuário
-	scanf("%s", cpf); //Armazena essa informação
-	
-	FILE *file;
-	file = fopen(cpf, "r"); //Abre o arquivo no modo de leitura para verificar se o mesmo existe
-	
-	if(file == NULL) //Se não existe exibe mensagem de erro ao usuário
+	do
 	{
-		printf("Usuário não encontrado\n"); //Mensagem de erro exibida para o usuário
-		system("pause"); //Pausa o programa até o usuário pressionar uma tecla
-	}
-	
-	else //Caso o arquivo exista, deleta o mesmo
+		
+		printf("Digite o CPF do usuário a ser apagado: "); //Coleta informação do usuário
+		scanf("%s", cpf); //Armazena essa informação
+		
+		FILE *file;
+		file = fopen(cpf, "r"); //Abre o arquivo no modo de leitura para verificar se o mesmo existe
+		
+		if(file == NULL) //Se não existe exibe mensagem de erro ao usuário
 		{
-			fclose(file); //Fecha o arquivo para que seja deletado
-			remove(cpf); //Deleta o arquivo
-			printf("Usuário apagado com sucesso!\n"); //Exibe mensagem de validação da ação para o usuário
-			system("pause"); //Pausa o sistema até o usuário pressionar uma tecla
+			printf("Usuário não encontrado\n"); //Mensagem de erro exibida para o usuário
+			system("pause"); //Pausa o programa até o usuário pressionar uma tecla
+			return 0;
 		}
-			
+		
+		else //Caso o arquivo exista, deleta o mesmo
+			{
+				fclose(file); //Fecha o arquivo para que seja deletado
+				remove(cpf); //Deleta o arquivo
+				printf("Usuário apagado com sucesso!\n"); //Exibe mensagem de validação da ação para o usuário
+				
+				printf("\nDeseja apagar outro usuário? (s/n): ");
+				scanf(" %c", &opcao);
+				system("cls");
+			}
+	}
+	while (opcao == 's');
+	return 0;			
 	
 }
 
